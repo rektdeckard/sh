04371b8b7beba4cb284c1fdcdf2ec69e3fe7101c
rektdeckard <friedtm@gmail.com>
2019-09-26 16:19:08 -0400

    Unlock desktop session remotely

--- knock-knock.sh
+++ knock-knock.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# knock-knock
-# Attach to display and unlock X session when connecting remotely
-# by Tobias Fried <friedtm@gmail.com>
-# created 2019.8.27
+## knock-knock
+## created 2019.8.27
+## author Tobias Fried <friedtm@gmail.com>
+
+## Attach to display and unlock X session when connecting remotely
 
 export DISPLAY=:0
 sudo loginctl unlock-sessions
